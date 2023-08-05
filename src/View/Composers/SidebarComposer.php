fddd203fd19ebeb8002ff9ddc3dbe73885e26621
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-08-05 12:00:16 +0200

    wip

--- src/View/Composers/SidebarComposer.php
+++ src/View/Composers/SidebarComposer.php
@@ -10,12 +10,11 @@ class SidebarComposer
     /**
      * Bind data to the view.
      *
-     * @param View $view
      * @return void
      */
     public function compose(View $view)
     {
-        if (!is_null(request()->route())) {
+        if (! is_null(request()->route())) {
             $pageName = request()->route()->getName();
             $routePrefix = explode('/', $pageName)[0] ?? '';
 
