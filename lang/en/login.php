de9a5cdd8211c9536f4771f2f6743a78964f40be
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-08-08 09:10:58 +0200

    wip

---
+++ lang/en/login.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => 'Login',
+    'welcome' => 'Welcome again',
+    'please-sign-in' => 'Please sign in below',
+    'email' => 'Email',
+    'enter-email' => 'Write your email',
+    'password' => 'Password',
+    'enter-password' => 'And your password here',
+    'remember-me' => 'Remember me',
+    'forgot-password' => 'Forgot password?',
+    'sign-in' => 'Log in'
+];
